a67988941ed0d350a7cbbc0573e74341973e5b82
DavidAMercer <33784314+DavidAMercer@users.noreply.github.com>
2020-09-16 15:12:06 -0230

    Lab 1

---
+++ Lab1/InterestArray.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Interest Array</title>
+	</head>
+	<body>
+		<?php
+		$RatesArray = array(
+		.0725,
+		.0750,
+		.0775,
+		.0800,
+		.0825,
+		.0850,
+		.0875
+		);
+
+		echo $RatesArray[0];
+		echo $RatesArray[1];
+		echo $RatesArray[2];
+		echo $RatesArray[3];
+		echo $RatesArray[4];
+		echo $RatesArray[5];
+		echo $RatesArray[6];
+		?>
+	</body>
+</html>
