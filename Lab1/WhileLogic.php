8ef90f82a83f8f81d6144ee498d0c758fa84500f
DavidAMercer <33784314+DavidAMercer@users.noreply.github.com>
2020-09-17 15:22:06 -0230

    Create WhileLogic.php

---
+++ Lab1/WhileLogic.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>While Logic</title>
+	</head>
+	<body>
+		<?php
+		$Count = 0;
+
+		while ($Count < 100) {
+			$Numbers[$Count] = $Count + 1;
+			++$Count;
+		}
+
+		foreach ($Numbers as $CurNum) {
+			echo "<p>$CurNum</p>";
+		}
+
+		?>
+	</body>
+</html> 
