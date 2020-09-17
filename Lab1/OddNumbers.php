476e8713abc69555905cea603317f36b2cdf2f98
DavidAMercer <33784314+DavidAMercer@users.noreply.github.com>
2020-09-17 15:05:08 -0230

    Create OddNumbers.php

---
+++ Lab1/OddNumbers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Odd Numbers</title>
+	</head>
+	<body>
+		<?php
+		$i = 1;
+		while ($i < 100) {
+			echo "$i<br />";
+			$i += 2;
+		}
+		?>
+	</body>
+</html> 
