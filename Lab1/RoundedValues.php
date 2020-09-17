2029362ae2c5b577e71878b814f5f9d7ad9e6b9a
DavidAMercer <33784314+DavidAMercer@users.noreply.github.com>
2020-09-16 22:13:40 -0230

    Create RoundedValues.php

---
+++ Lab1/RoundedValues.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Rounded Values</title>
+	</head>
+	<body>
+		<?php
+		$num1 = 3.4;
+		$num2 = 7.2;
+		$num3 = 6.8;
+
+		echo round($num1) . "<br />";
+		echo ceil($num2) . "<br />";
+		echo floor($num3);
+
+		?>
+	</body>
+</html> 
