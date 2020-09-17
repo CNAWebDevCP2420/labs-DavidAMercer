6dc31e0445a3c2144fcf83a469aa07b73bba1e73
DavidAMercer <33784314+DavidAMercer@users.noreply.github.com>
2020-09-17 14:04:33 -0230

    Create ConditionalScript.php

---
+++ Lab1/ConditionalScript.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Conditional Script</title>
+	</head>
+	<body>
+		<?php
+		$IntVariable = 75;
+		if ($IntVariable > 100)
+			$Result  = "$IntVariable is greater than 100";
+		else
+			$Result = "$IntVariable is less than or equal to 100";
+		echo $Result;
+		?>
+	</body>
+</html> 
